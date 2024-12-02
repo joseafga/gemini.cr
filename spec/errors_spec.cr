03879e9099fdf3f3fb7a177afcbae30529f4c2ad
José Almeida <jose.afga@gmail.com>
2024-12-02 04:02:01 -0300

    Use `expect_raises` for errors spec

--- spec/errors_spec.cr
+++ spec/errors_spec.cr
@@ -1,31 +1,31 @@
 require "./spec_helper"
 
 describe Gemini do
-  it "handle PROHIBITED_CONTENT error", tags: "error" do
-    Gemini::GenerateContentResponse.from_json SAMPLES["prohibited_content_00"]
+  it "exception by block reason: PROHIBITED_CONTENT", tags: "error" do
+    ex = expect_raises(Gemini::MissingCandidatesException) do
+      Gemini::GenerateContentResponse.from_json SAMPLES["prohibited_content_00"]
+    end
 
-    raise "No error triggered"
-  rescue ex : Gemini::MissingCandidatesException
-    # puts "Rescued `Gemini::MissingCandidatesException` -- #{ex.message} -- Block Reason: #{ex.block_reason}"
     ex.block_reason.should eq Gemini::BlockReason::ProhibitedContent
   end
 
-  it "handle finish reason by SAFETY error", tags: "error" do
-    Gemini::GenerateContentResponse.from_json SAMPLES["finish_reason_safety_00"]
+  it "exception by finish reason: SAFETY", tags: "error" do
+    ex = expect_raises(Gemini::MissingContentException) do
+      Gemini::GenerateContentResponse.from_json SAMPLES["finish_reason_safety_00"]
+    end
 
-    raise "No error triggered"
-  rescue ex : Gemini::MissingContentException
-    # puts "Rescued `Gemini::MissingContentException` -- #{ex.message} -- Finish Reason: #{ex.finish_reason}"
     ex.finish_reason.should eq Gemini::FinishReason::Safety
   end
 
-  it "bad request error", tags: "error" do
-    begin
-      Gemini::GenerateContentResponse.from_json SAMPLES["bad_request_error_00"]
-    rescue ex : JSON::SerializableError
+  it "exception by bad request", tags: "error" do
+    ex = expect_raises(Gemini::BadResponseException) do
+      expect_raises(JSON::SerializableError) do
+        Gemini::GenerateContentResponse.from_json SAMPLES["bad_request_error_00"]
+      end
+
       raise Gemini::BadResponseException.new "Can't parse JSON response", SAMPLES["bad_request_error_00"]
     end
-  rescue ex : Gemini::BadResponseException
+
     ex.error.try &.code.should eq 400
     ex.error.try &.status.should eq "INVALID_ARGUMENT"
   end
